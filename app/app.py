fa9f2ac91e6e555522fb5d0edbd4417f6f20b791
ayankashyap <ayankashyap8@gmail.com>
2020-10-27 20:33:12 +0530

    switched database to sqlite3

--- app/app.py
+++ app/app.py
@@ -1,33 +1,47 @@
-from flask import Flask
-from typing import List, Dict
-import mysql.connector
-import json
+from flask import Flask, render_template, request
+import sqlite3 as sql
 
 app = Flask(__name__)
 
-
-def display() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'mynewpassword',
-        'host': 'db',
-        'port': '3306',
-        'database': 'userdata'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM users')
-    results = [{username: email} for (username, email) in cursor]
-    cursor.close()
-    connection.close()
-
-    return results
-
-
 @app.route('/')
-def index() -> str:
-    return json.dumps({'userdata': display()})
+def home():
+    return render_template('home.html')
 
+@app.route('/new')
+def new():
+    return render_template('user.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        try:
+            username = request.form['username']
+            email = request.form['email']
+
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+
+                cur.execute("INSERT INTO users (username, email) VALUES (?,?)", (username, email))
+                con.commit()
+                msg = "Record added"
+        except:
+            con.rollback()
+            msg = "Error in insertion"
+        
+        finally:
+            return render_template("result.html", msg = msg)
+            con.close()
+
+@app.route('/disp')
+def display():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+   
+    cur = con.cursor()
+    cur.execute("select * from users")
+   
+    rows = cur.fetchall()
+    return render_template("display.html",rows = rows)
+
+if __name__ == "__main__":
+    app.run(debug = True)
